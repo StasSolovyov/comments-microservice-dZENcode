d20cd6cdc86fb62248fd3e6d33d0988610db278e
StasSolovyov <149395885+StasSolovyov@users.noreply.github.com>
2024-05-01 12:17:31 +0300

    new

---
+++ src/config/awsConfig.js
@@ -0,0 +1,13 @@
+const AWS = require('aws-sdk');
+
+// Конфигурация AWS SDK
+AWS.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION,
+});
+
+const sqs = new AWS.SQS({ apiVersion: '2012-11-05' });
+const queueUrl = process.env.QUEUE_URL;
+
+module.exports = { sqs, queueUrl };
